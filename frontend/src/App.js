26fea2664ba2cc6fd9b823a86886f87916ff67dd
DannyYang <yjring@gmail.com>
2023-06-06 14:30:40 +0800

    add frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Actions!
         </a>
       </header>
     </div>
