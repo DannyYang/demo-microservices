9017e18e8db2155bb12ab0c0531e613e58664f35
DannyYang <yjring@gmail.com>
2023-06-06 14:38:17 +0800

    modify frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Actions!
+          Hello Actions! v2
         </a>
       </header>
     </div>
