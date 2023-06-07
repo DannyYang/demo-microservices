87ce8a3a132d5bd608381f719f1de3b9d1648a4c
DannyYang <yjring@gmail.com>
2023-06-07 15:17:32 +0800

    modify somethings

--- backend/src/main/java/com/webcomm/tw/svc/vote/controller/VoteController.java
+++ backend/src/main/java/com/webcomm/tw/svc/vote/controller/VoteController.java
@@ -7,7 +7,6 @@ import com.webcomm.tw.svc.vote.vote.VoteRecord;
 import com.webcomm.tw.svc.vote.vote.VoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
