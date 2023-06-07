bfd84da1d96be4af3156fc4d11b9e0853b9a8b5e
Cherry <cherry@webcomm.com.tw>
2023-06-07 21:44:50 +0800

    rewrote code to make it better

--- frontend/src/components/ColorfulBackground.js
+++ frontend/src/components/ColorfulBackground.js
@@ -1,52 +1,47 @@
-import { useState, useMemo } from 'react'
+import { useMemo } from 'react';
+
+const containerStyle = { 
+    position: 'relative', 
+    display: 'flex', 
+    width: '100%', 
+    height: '50vh' 
+};
+
+const labelStyle = { 
+    backgroundColor: 'white', 
+    textAlign: 'center'
+};
 
 function ColorfulBackground({ results, options }) {
-    const mergedArray = useMemo(() => {
-        if (results.length !== 0) {
-            const array = options.map((option) => {
-                const countObject = results.find((result) => result.optionId === option.optionId);
-                return { ...option, count: countObject?.count || 0 };
-            });
-            return array;
-        } else {
-            return [];
-        }
+    const { mergedArray, totalCount } = useMemo(() => {
+        const array = options.map((option) => {
+            const countObject = results.find((result) => result.optionId === option.optionId);
+            const count = countObject?.count || 0;
+            return { ...option, count };
+        });
+        const count = array.reduce((sum, option) => sum + option.count, 0);
+        return { mergedArray: array, totalCount: count };
     }, [results, options]);
 
-    const totalCount = mergedArray.reduce((sum, option) => sum + option.count, 0);
-    
-
-    // const renderedMap = mergedArray.map(({ count, label }) => {
-    //     return <tr className='table'>
-    //         {label}
-    //         {( Math.floor((count / totalCount) * 100) + '%')}
-    //     </tr>;
-    // });
-    // return (<table><tbody>{renderedMap}</tbody></table>);
-
     return (
-        <div style={{ position: 'relative', 
-                        display: 'flex', 
-                        // flexDirection: 'column', 
-                        width: '100%', height: '50vh' }}>
+        <div style={containerStyle}>
             {mergedArray.map(({ bgColor, count, label }, index) => {
-                    return (
-                    <div key={index} style={{
-                        backgroundColor: bgColor,
-                        flexBasis: `${(count / totalCount) * 100}%`, }}>
-                        <div style={{ 
-                                backgroundColor: 'white', 
-                                textAlign: 'center',
-                            }}>
-                            {count ? ( Math.floor((count / totalCount) * 100) + '%') : ''}
-                            {count == 0 ? '' : label}
+                const percent = count ? Math.floor((count / totalCount) * 100) + '%' : '';
+                const blockStyle = {
+                    backgroundColor: bgColor,
+                    flexBasis: `${(count / totalCount) * 100}%`
+                };
+                return (
+                    <div key={index} style={blockStyle}>
+                        <div style={labelStyle}>
+                            {percent}
+                            {count !== 0 && label}
                         </div>
                     </div>
-                    )
-                })
-            }
+                );
+            })}
         </div>
     );
-    
 }
+
 export default ColorfulBackground;
